addb6d37ad92f4783f1926b945d89c30e3be765b
Anshul Agarwal <ansh9918447472@gmail.com>
2026-08-06 18:11:15 +0530

    Align desktop top bar with app shell

--- Frontend/components/app-shell/top-bar.tsx
+++ Frontend/components/app-shell/top-bar.tsx
@@ -32,7 +32,7 @@ export function TopBar({
 
   return (
     <header className="fixed left-0 right-0 top-0 z-40 h-16 border-b border-border/50 bg-background/80 glass">
-      <div className="mx-auto flex h-full max-w-7xl items-center justify-between px-4 lg:px-6">
+      <div className="flex h-full w-full items-center justify-between px-4 lg:px-6">
         {/* Logo */}
         <AppLogo />
 
